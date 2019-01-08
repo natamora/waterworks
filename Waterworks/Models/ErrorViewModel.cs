96f637fd7201ee795796a1b0a93be6194ee818b8
Kasia Morawska <natamora96@gmail.com>
2019-01-08 13:05:43 +0100

    Add project files.

---
+++ Waterworks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Waterworks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
